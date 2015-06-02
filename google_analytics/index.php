bda2e12651e7e89b9dac0816369c96af2e6639e3
Shigeaki Matsumura <matsu911@gmail.com>
2015-06-02 18:21:26 +0900

    moved analytics code from footer to head

--- google_analytics/index.php
+++ google_analytics/index.php
@@ -54,10 +54,10 @@ Plugin update URI: http://www.osclass.org/files/plugins/google_analytics/update.
     /**
      * This function is called every time the page footer is being rendered
      */
-    function google_analytics_footer() {
+    function google_analytics_header() {
         if( osc_google_analytics_id() != '' ) {
             $id = osc_google_analytics_id();
-            require_once(osc_plugins_path() . 'google_analytics/footer.php');
+            require_once(osc_plugins_path() . 'google_analytics/header.php');
         }
     }
 
@@ -70,7 +70,7 @@ Plugin update URI: http://www.osclass.org/files/plugins/google_analytics/update.
     // This is a hack to show a Uninstall link at plugins table (you could also use some other hook to show a custom option panel)
     osc_add_hook(osc_plugin_path(__FILE__)."_uninstall", 'google_analytics_call_after_uninstall');
     osc_add_hook(osc_plugin_path(__FILE__)."_configure", 'google_analytics_admin');
-    osc_add_hook('footer', 'google_analytics_footer');
+    osc_add_hook('header', 'google_analytics_header');
     osc_add_hook('admin_menu_init', 'google_admin_menu');
 
 ?>
